1d988bfdd62b1ad2e0b51025f9b3ad2dc8658b35
Vy Nguyen <vynguyen205@Vys-MacBook-Air.attlocal.net>
2022-04-17 23:07:49 -0700

    added more codes to show questions and hide home

--- README.md
+++ README.md
@@ -13,3 +13,22 @@ All you need to have prior to taking this quiz is your basic knowlege of Javascr
 ‼️ Please note. If you get a question wrong, time will be subtracted from your clock. So, answer deligently. (It's actually not that serious.)
 
 Once you are done, write your initials and keep track of your own score. 
+
+
+
+//timer and subtract time when asnwered wrong
+var timeEl = document.querySelector('timer-sec');
+var counter = 30;
+
+var countDown = setInterval(function(){
+    counter--;
+    timeEl.textContent = counter + 'seconds';
+    console.log(countDown);
+    // if (counter <= 0){ 
+    //     timeEl.textContent = "GAME OVER";
+    clearInterval(countDown);
+    // }
+}, 1000);
+
+//add buttons events here
+startBtn.addEventListener("click", countDown())
--- assets/script.js
+++ assets/script.js
@@ -1,57 +1,121 @@
 var startBtn = document.getElementById('start');
+var scoreBtn = document.querySelector('.scoreBtn');
 
+var q1 = document.getElementById('show-question');
+var quizInfo= document.querySelector('.quiz-info')
+
+var currentQuestionIndex= 0
 
 //Questions for the quiz
 var myQuestions = [
- {
-    question1: "Which one of these is a data type that we learned in class?",
-    answers: {
-        a: "rope",
-        b: "string",
-        c: "integer",
+    {
+       question: "Which one of these is a data type that we learned in class?",
+       answers: {
+           a: "rope",
+           b: "string",
+           c: "integer",
+       },
+       correctAnswer: "b"
     },
-    correctAnswer: "b"
- },
- {
-    question1: "Everything in Javascipt is a(n)...",
-    answers: {
-        a: "funtion",
-        b: "element",
-        c: "object",
+    {
+       question: "Everything in Javascipt is a(n)...",
+       answers: {
+           a: "funtion",
+           b: "element",
+           c: "object",
+       },
+       correctAnswer: "c"
     },
-    correctAnswer: "c"
- },
- {
-    question1: "What do you need to do to a delcared function for it to work?",
-    answers: {
-        a: "retun it",
-        b: "call it",
-        c: "console.log",
-    },
-    correctAnswer: "b"
- },      
+    {
+       question: "What do you need to do to a delcared function for it to work?",
+       answers: {
+           a: "retun it",
+           b: "call it",
+           c: "console.log",
+       },
+       correctAnswer: "b"
+    },      
+   
+   ];
 
-];
+//StartQuiz
+function startQuiz () {
+    startBtn.style.display = 'none';
+    quizInfo.style.display = 'none';
 
-//how to display the question on the page
-
-function showQuestion(q) {
-}
-
-if (myQuestions) {
-    (question1 === "b")
+    showQuestions();
+    
+    startTimer();
     
 }
+//need to show question on HTML
+function showQuestions(){
+    q1.innerHTML = ""; //need to clear previous question
+    var q = myQuestions[currentQuestionIndex]; // this is how we can track current question 
+    var qTitleEl= document.createElement("h1");
+    qTitleEl.textContent= q.question; // assign the question to the h1 tag we just created
+    q1.append(qTitleEl); // this will put the question on the HTML page
+   
+    // show answers and put it on page
+    var answers = q.answers // easier to access answers above
+    for (var i = 0; i < answers.length; i++) {
+        var answer = answers[i]; //loop through each answers in the answer array
+        var button = document.createElement('button');
+        button.textContent = answer;
+        q1.append(button);
+        // button.addEventListener("click", function(event) {
+        //     var selectedAnswer = event.target.textContent
+        //     alert("YOU CHOSE " + selectedAnswer);
+            
+        //     if (selectedAnswer === q.correctAnswer) {
+        //         answerCorrect();
+        //     }else {
+        //         answerWrong();
+        //     }
 
-showQuestion(myQuestions);
+        //     //go to next question
+        //     currentQuestionIndex++;
+        //     showQuestions();
+        // })
+    }
+}
 
-//add buttons events here
-startBtn.addEventListener("click", showQuestion())
 
-//submit button for the quiz and jump to next question
 
-//a pop up for when the answer is correct or wrong
+//if answer correct
+function answerCorrect() {
+    var displayStatus = document.createElement("div");
+    displayStatus.textContent = "Correct";
+    q1.append(displayStatus);
+    return
+}
+
+
+//if answer wrong
+function answerWrong() {
+    var displayStatus = document.createElement("div");
+    displayStatus.textContent = "Wrong";
+    q1.append(displayStatus);
+    
+    //subtract time from clock
+    counter -= 10;
+}
 
 //timer and subtract time when asnwered wrong
+var timeEl = document.querySelector('.count-down');
+var counter = 30; // change this later
 
-//be able to save intials and store in localstorage
+var countDown; 
+
+function startTimer () {
+    countDown = setInterval(function(){
+        counter--;
+        timeEl.textContent = 'Time left: ' + counter + ' s';
+        if (counter <= 0){ 
+            timeEl.textContent = "GAME OVER";
+        clearInterval(countDown);
+        }
+    }, 1000);
+}
+//add buttons events here
+startBtn.addEventListener("click", startQuiz);  
