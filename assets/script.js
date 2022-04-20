9190559c06f69ddd951b7ff3256f27805fca87ba
Vy Nguyen <vynguyen205@Vys-MacBook-Air.home>
2022-04-20 12:48:16 -0700

    updated JS

--- assets/script.js
+++ assets/script.js
@@ -2,10 +2,11 @@ var startBtn = document.getElementById('start');
 var scoreBtn = document.querySelector('.scoreBtn');
 
 var q1 = document.getElementById('show-question');
-var quizInfo= document.querySelector('.quiz-info')
-var resultPage = document.getElementById('result-page')
+var quizInfo= document.querySelector('.quiz-info');
+var resultPage = document.getElementById('result-page');
+var answerStatus = document.querySelector('.answer-status');
 
-var currentQuestionIndex= 0
+var currentQuestionIndex= 0;// this is how we can track current question 
 
 //Questions for the quiz
 var myQuestions = [
@@ -16,7 +17,7 @@ var myQuestions = [
            "b: string",
            "c: integer",
        ],
-       correctAnswer: "b"
+       correctAnswer: "b: string"
     },
     {
        question: "Everything in Javascipt is a(n)...",
@@ -25,16 +26,16 @@ var myQuestions = [
            "b: element",
            "c: object",
        ],
-       correctAnswer: "c"
+       correctAnswer: "c: object"
     },
     {
        question: "What do you need to do to a delcared function for it to work?",
        answers: [
-           "a: retun it",
+           "a: return it",
            "b: call it",
            "c: console.log",
        ],
-       correctAnswer: "b"
+       correctAnswer: "b: call it"
     },      
    
    ];
@@ -43,24 +44,28 @@ var myQuestions = [
 function startQuiz () {
 
     hideHome();
-    
-    showQuestions();
-    
     startTimer();
+    showQuestions();
     
 }
 
 //hide home page
-function hideHome () {
+function hideHome() {
     startBtn.style.display = 'none';
     quizInfo.style.display = 'none';
 }
+
+//hide questions page
+
+// function seeScoreboard() {
+//     q1.style.display = 'none';
+// }
+var q = myQuestions[currentQuestionIndex];
 //need to show question on HTML
 function showQuestions(){
     q1.innerHTML = ""; //need to clear previous question
-    var q = myQuestions[currentQuestionIndex]; // this is how we can track current question 
     var qTitleEl= document.createElement("h1");
-    qTitleEl.textContent= q.question; // assign the question to the h1 tag we just created
+    qTitleEl.textContent = q.question; // assign the question to the h1 tag we just created
     q1.append(qTitleEl); // this will put the question on the HTML page
 
     // show answers and put it on page
@@ -71,44 +76,63 @@ function showQuestions(){
         var button = document.createElement('button');
         button.textContent = answer;
         q1.append(button);
-        button.addEventListener("click", function(event) {
-                var selectedAnswer = event.target
-                // alert("YOU CHOSE " + selectedAnswer);
+        button.addEventListener("click", clickAnswer);
+        //         var selectedAnswer = event.target;
             
-                if (selectedAnswer === q.correctAnswer) {
-                        answerCorrect();
-                    }else {
-                        answerWrong();
-                        }
-                    
-                //go to next question
-                currentQuestionIndex++;
-                showQuestions();
-            });
+        //         if (selectedAnswer === q.correctAnswer) {
+        //                 answerCorrect();
+        //             }else (selectedAnswer !== q.correctAnswer)
+        //                 answerWrong();
+        //         //go to next question
+        //         currentQuestionIndex++;
+        //         showQuestions();
+
+        //     });
     }
 }
-            
+
+function clickAnswer (event) {
+    var selectedAnswer = event.target;
+    
+    if (selectedAnswer === q.correctAnswer) {
+        answerCorrect();
+    }
+    
+    currentQuestionIndex++;
+    showQuestions();
+}
+           
 //if answer correct
 function answerCorrect() {
-    
+    answerStatus = "";
+    setTimeout (function() {
+        answerStatus.textContent = "Correct!"   
+    },1000);
 }
-
 
 //if answer wrong
 function answerWrong() {
-   
-    
+    answerStatus = "";
+    setTimeout (function() {
+        answerStatus.textContent = "Wrong!"
+    },1000);
     //subtract time from clock
     counter -= 10;
 }
 
-//when the timer is over
-// function gameOver() {
-//     var endPage = document.createElement('h1');
-//     endPage.textContent = "Highscores";
-//     resultPage.append(endPage);
+// when the timer is over
+function gameOver() {
+    var endPgTitle = document.createElement('h1');
+    endPgTitle.textcontent = 'SCOREBOARD';
+    resultPage.append(endPgTitle);
 
-// }
+    var endText = document.createElement('h3');
+    endText.textContent = 'YOUR CURRENT SCORE IS: ';
+    resultPage.append(endText);
+
+}
+
+
 //timer and subtract time when asnwered wrong
 var timeEl = document.querySelector('.count-down');
 var counter = 30; // change this later
@@ -118,10 +142,11 @@ var countDown;
 function startTimer () {
     countDown = setInterval(function(){
         counter--;
-        timeEl.textContent = 'Time left: ' + counter + ' s';
+        timeEl.textContent = 'Time left: ' + counter + ' S';
         if (counter <= 0){ 
             timeEl.textContent = "GAME OVER";
-        clearInterval(countDown);
+            clearInterval(countDown);
+            gameOver();
         }
 
     }, 1000);
